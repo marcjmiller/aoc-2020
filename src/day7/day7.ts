6069eeb6b911c940ef65a647531cc04852ccf91d
Marc Miller <marc.miller.4@us.af.mil>
2020-12-07 16:15:32 -0500

    eod: need to finish another time

--- src/day7/day7.ts
+++ src/day7/day7.ts
@@ -1,6 +1,6 @@
-import { testData, inputData } from './input';
+import { testDataPart1, testDataPart2, inputData } from './input';
 
-function splitInputIntoRules(data: string) {
+function splitInputIntoRulesMap(data: string) {
   const rulesAsStr = data.split('\n');
   const rulesMap = new Map<string, string[]>();
 
@@ -29,8 +29,16 @@ function splitInputIntoRules(data: string) {
   return rulesMap;
 }
 
-function main(data: string) {
-  const rules = splitInputIntoRules(data);
+function getBags(data: string) {
+  let bags: string[][] = [];
+  data.split('.\n').map((line) => {
+    bags.push(line.match(/^(?:\w+\s\w+)|([0-9] \w+\s\w+)/g) || []);
+  });
+  return bags;
+}
+
+function part1(data: string) {
+  const rules = splitInputIntoRulesMap(data);
   let count = 0;
   rules.forEach((bagsList) => {
     if (bagsList.includes('shiny gold')) {
@@ -40,8 +48,24 @@ function main(data: string) {
   return count;
 }
 
-const testDataCount = main(testData);
-console.log(`Test data count should be 4: ${testDataCount === 4 ? 'PASS' : 'FAIL'}`);
+// TODO: Finish part2 when able.
+function part2(data: string) {
+  let countChildren = 0;
+  const bags = getBags(data);
 
-const liveDataCount = main(inputData);
-console.log(`Live data count is: ${liveDataCount}`);
+  console.log(bags);
+
+  return countChildren;
+}
+
+// const testDataCountPart1 = part1(testDataPart1);
+// console.log(`Test data part 1: ${testDataCountPart1 === 4 ? 'PASS' : 'FAIL'}\n`);
+
+// const liveDataCountPart1 = part1(inputData);
+// console.log(`Live data count is: ${liveDataCountPart1}\n`);
+
+const testDataCountPart2Ex1 = part2(testDataPart1);
+console.log(`Test data part 2: ${testDataCountPart2Ex1} === 32 ? 'PASS' : 'FAIL'\n`);
+
+const testDataCountPart2Ex2 = part2(testDataPart2);
+console.log(`Test data part 2: ${testDataCountPart2Ex2} === 126 ? 'PASS' : 'FAIL'\n`);
