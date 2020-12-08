339bce0d5a983a735491fe368d4e9533e89a6c85
Marc Miller <marc.miller.4@us.af.mil>
2020-12-08 12:32:48 -0500

    day8 part2 completed

--- src/day8/day8.ts
+++ src/day8/day8.ts
@@ -1,23 +1,39 @@
 import { testData, liveData } from './input';
 
+enum ExitCode {
+  'EOF' = 0,
+  'LOOP' = 1,
+  'ERROR' = 2,
+}
+
+type Result = {
+  operations: number[];
+  accumulator: number;
+  exitCode: ExitCode;
+};
+
 function parseBootCodeToInstructions(bootCode: string) {
   return bootCode.split('\n');
 }
 
-function runBootCode(code: string[]) {
+function runBootCode(code: string[]): Result {
   let instruction = 0;
   let accumulator = 0;
   let operations: number[] = [];
+  let exitCode: ExitCode;
 
   while (true) {
-    const command = code[instruction].slice(0, 3);
-    const argument = +code[instruction].slice(4);
-
     if (operations.includes(instruction)) {
+      exitCode = ExitCode.LOOP;
+      break;
+    } else if (instruction >= code.length) {
+      exitCode = ExitCode.EOF;
       break;
     }
 
-    // console.log(`performing ${command} with arg ${argument}`);
+    const command = code[instruction].slice(0, 3);
+    const argument = +code[instruction].slice(4);
+
     operations.push(instruction);
 
     switch (command) {
@@ -33,26 +49,72 @@ function runBootCode(code: string[]) {
     }
   }
 
-  const result: [number[], number] = [operations, accumulator];
+  let result: Result = { operations, accumulator, exitCode };
   return result;
 }
 
-function logResult(result: number, desc: string, testCond?: number) {
-  console.log(`${desc} accumulator value: ${result}`);
+function logResult(result: Result, desc: string, testCond?: number, testExitCode?: ExitCode) {
+  console.log(`Completed ${result.operations.length} operations.`);
 
   if (testCond) {
-    console.log(`  ${result === testCond ? '\u2713 PASS' : '\u2716 FAIL'}`);
+    console.log(
+      `  Accumulator Value should be ${testCond}: ${result.accumulator === testCond ? '\u2713 PASS' : '\u2716 FAIL'}`
+    );
+  } else {
+    console.log(`${desc} accumulator value: ${result.accumulator}`);
+  }
+
+  if (testExitCode) {
+    console.log(
+      `  Exit Code should be ${testExitCode}: ${result.exitCode === testExitCode ? '\u2713 PASS' : '\u2716 FAIL'}`
+    );
+  } else {
+    console.log(`${desc} exit code: ${result.exitCode}`);
   }
   console.log('\n');
 }
 
-function main(bootCode: string) {
-  const bootInstructions = parseBootCodeToInstructions(bootCode);
-  const result = runBootCode(bootInstructions);
-
-  return result[1];
+function swapInstruction(instruction: string) {
+  if (instruction.includes('nop')) {
+    return instruction.replace('nop', 'jmp');
+  } else {
+    return instruction.replace('jmp', 'nop');
+  }
 }
 
-logResult(main(testData), 'testData', 5);
+function fixBootCode(code: string[]): Result {
+  const possibleChanges: [number, string][] = [];
 
-logResult(main(liveData), 'liveData');
+  code.map((line, index) => {
+    (line.includes('nop') || line.includes('jmp')) && possibleChanges.push([index, line]);
+  });
+
+  let result = runBootCode(code);
+  for (let change of possibleChanges) {
+    let newCode = Array.from(code);
+
+    newCode[change[0]] = swapInstruction(newCode[change[0]])
+
+    result = runBootCode(newCode);
+    if (result.exitCode === ExitCode.EOF) {
+      break;
+    }
+  }
+
+  return result;
+}
+
+function main(bootCode: string, part2?: boolean) {
+  const bootInstructions = parseBootCodeToInstructions(bootCode);
+  const result = part2 ? fixBootCode(bootInstructions) : runBootCode(bootInstructions);
+
+  return result;
+}
+
+logResult(main(testData), 'testData part 1', 5, ExitCode.LOOP);
+
+logResult(main(liveData), 'liveData part 1', undefined, ExitCode.LOOP);
+
+logResult(main(testData, true), 'testData part 2', 8, ExitCode.EOF);
+
+logResult(main(liveData, true), 'liveData part 2', undefined, ExitCode.EOF)
